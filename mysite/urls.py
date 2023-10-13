c21bf3826f1aec93d6b26255ff28ba4ff0b90266
ROFL1ST <danendrapr55@gmail.com>
2023-10-13 08:26:05 +0700

    P

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from blog import views as views_blog
+from about import views as about_views
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("about/", views.about),
+    path("about/", about_views.index ),
     path("blog/", views_blog.index),
     path("", views.index)
 ]
